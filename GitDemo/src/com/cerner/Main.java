6f632e6080e97c9efde1fd1ec1e65b88439b627b
neha singh <singhgimmy.24@gmail.com>
2014-07-22 04:39:43 -0700

    Intial checkin

---
+++ GitDemo/src/com/cerner/Main.java
@@ -0,0 +1,15 @@
+package com.cerner;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+data d=new data();
+d.disp();
+
+	}
+
+}
---
+++ GitDemo/src/com/cerner/data.java
@@ -0,0 +1,9 @@
+package com.cerner;
+
+public class data {
+
+	public void disp(){
+		System.out.println("in data");
+				
+	}
+}
